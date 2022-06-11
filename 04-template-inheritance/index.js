44765c6d166536c3277a43e8b7788fd4a857e256
kunxin-chor <chorkunxin@yahoo.com>
2022-06-11 05:53:04 +0000

    Added helpers

--- 04-template-inheritance/index.js
+++ 04-template-inheritance/index.js
@@ -8,6 +8,17 @@ app.set('view engine', 'hbs'); // using hbs as our view engine
 wax.on(hbs.handlebars);  // add template inheritance capacity to hbs
 wax.setLayoutPath('./views/layouts'); // where to find layouts (layouts = common hbs file that can be shared)
 
+// custom helpers
+hbs.handlebars.registerHelper('ifEquals', function(arg1, arg2, options){
+    if (arg1 == arg2) {
+        // display the part before the {{else}}
+        return options.fn(this);
+    } else {
+        // display the partr after the {{else}}
+        return options.inverse(this);
+    }
+})
+
 // routes here
 app.get('/', function(req,res){
     res.render('home')
@@ -19,6 +30,19 @@ app.get('/about-us', function(req,res){
 
 app.get('/contact-us', function(req,res){
     res.render('contact-us')
+})
+
+app.get('/products', function(req,res){
+    let products = ["Organic Leather Shoes", 
+                    "Organic Cotton Shirt", 
+                    "Organic Cotton Socks", 
+                    "Cap"];
+
+    let discount = true;
+    res.render('products',{
+        'products':products,
+        'discount':discount
+    })
 })
 
 app.listen(3000, function(){
